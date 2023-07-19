ab4b6e18cd39cf0939a77cacbf4a5da16743e971
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-19 13:14:49 -0400

    html parsing working

--- soup-parser.py
+++ soup-parser.py
@@ -1,14 +1,18 @@
-with open("prizelist.txt") as file:
+with open("prizelist.html") as file:
     contents = file.read()
     contents = contents.splitlines()
-
     csv = ""
-    for i in range(0, len(contents)):
-        if i % 2 == 1:
-            temp = "\'" + contents[i-1] + "\', \'" + contents[i] + "\'"
-            if i != len(contents):
-                temp += "\n"
-            csv += temp
-print(csv)
-with open("prizelist.csv", "w") as f:
-    f.write(csv)
+    temp = ""
+
+    for line in contents:
+        if line.__contains__("&nbsp;&nbsp;&nbsp;&nbsp;"):
+            temp = line.replace("                            &nbsp;&nbsp;&nbsp;&nbsp;", "")
+            print(temp)
+            # if i % 2 == 1:
+            #     temp = "\'" + contents[i-1] + "\', \'" + contents[i] + "\'"
+            #     if i != len(contents):
+            #         temp += "\n"
+            #     csv += temp
+# print(csv)
+# with open("temp.csv", "w") as f:
+#     f.write(csv)
