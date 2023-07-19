de31d5d3303097fed7e7fbcd338309cf523ef4f8
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-19 13:45:53 -0400

    made script write output to file

--- soup-parser.py
+++ soup-parser.py
@@ -1,18 +1,28 @@
-with open("prizelist.html") as file:
-    contents = file.read()
-    contents = contents.splitlines()
-    csv = ""
-    temp = ""
+indicator = "                            &nbsp;&nbsp;&nbsp;&nbsp;"
 
-    for line in contents:
-        if line.__contains__("&nbsp;&nbsp;&nbsp;&nbsp;"):
-            temp = line.replace("                            &nbsp;&nbsp;&nbsp;&nbsp;", "")
-            print(temp)
-            # if i % 2 == 1:
-            #     temp = "\'" + contents[i-1] + "\', \'" + contents[i] + "\'"
-            #     if i != len(contents):
-            #         temp += "\n"
-            #     csv += temp
-# print(csv)
-# with open("temp.csv", "w") as f:
-#     f.write(csv)
+def dump_relevant_lines(file, indicator):
+    temp = []
+    with open(file) as file:
+        contents = file.read()
+        contents = contents.splitlines()
+
+        for line in contents:
+            if line.__contains__(indicator):
+                temp.append(line.replace(indicator, ""))
+    return temp
+
+def convert_to_csv(temp):
+    csv = ""
+    for i in range(0,len(temp)):
+        if i % 2 == 1 or i == 1:
+            csv += "\'" + temp[i-1] + "\', \'" + temp[i] + "\'"
+            if i < len(temp) - 1:
+                csv += "\n"
+    return csv
+
+csv = convert_to_csv(dump_relevant_lines("prizelist.html", indicator))
+
+print(csv)
+
+with open("prizelist.csv", "w") as f:
+    f.write(csv)
