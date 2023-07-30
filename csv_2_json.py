ccf3b82d16289c1fa48caaf48ee6fffbdc3351fd
Kieran Klukas - kierank@Glaedrs-Nest.local <92754843+kcoderhtml@users.noreply.github.com>
2023-07-30 10:07:58 -0400

    :sparkles: Transform CSV to JSON in csv_2_json.py

--- csv_2_json.py
+++ csv_2_json.py
@@ -3,30 +3,29 @@ import csv
 def transform_csv_to_json(csv_file):
     json_data = {}
 
-    with open(csv_file) as csvfile:
-        reader = csv.reader(csvfile, skipinitialspace=True)
-        for row in reader:
-            names = row[0]
-            event = row[1]
-            names = names.split(', ')
-            
-            if len(names) < 2:
-                print(f"Invalid name format: {names}. Skipping...")
-                print(f"Event: {event}")
-                continue
-            
-            family_name = names[0]
-            first_name = names[1].split(' ')[0]
+    reader = csv.reader(csv_file, skipinitialspace=True)
+    for row in reader:
+        names = row[0]
+        event = row[1]
+        names = names.split(', ')
+        
+        if len(names) < 2:
+            print(f"Invalid name format: {names}. Skipping...")
+            print(f"Event: {event}")
+            continue
+        
+        family_name = names[0]
+        first_name = names[1].split(' ')[0]
 
-            if 'family' not in json_data:
-                json_data['family'] = {}
-            
-            if family_name not in json_data['family']:
-                json_data['family'][family_name] = {}
-            
-            if first_name not in json_data['family'][family_name]:
-                json_data['family'][family_name][first_name] = []
-            
-            json_data['family'][family_name][first_name].append(event.strip())
+        if 'family' not in json_data:
+            json_data['family'] = {}
+        
+        if family_name not in json_data['family']:
+            json_data['family'][family_name] = {}
+        
+        if first_name not in json_data['family'][family_name]:
+            json_data['family'][family_name][first_name] = []
+        
+        json_data['family'][family_name][first_name].append(event.strip())
 
     return json_data
